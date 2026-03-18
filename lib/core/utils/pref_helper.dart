312d087d953a2637b8aa1d95aafb72bf145123f3
Mohamed Eltokhy <eltokeymohamed444@email.com>
2026-03-18 07:02:08 +0200

    Add Arabic and English translation files and implement PrefHelper for token management

---
+++ lib/core/utils/pref_helper.dart
@@ -0,0 +1,31 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class PrefHelper {
+  static const String _tokenKey = 'auth_token';
+  static const String _isFirstLaunch = 'is_first_launch'; 
+
+  static Future<void> saveToken(String token) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_tokenKey, token);
+  }
+
+  static Future<String?> getToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_tokenKey);
+  }
+
+  static Future<void> clearToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(_tokenKey);
+  }
+
+    static Future<bool> isFirstLaunch() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getBool(_isFirstLaunch) ?? true;
+  }
+
+  static Future<void> setLaunched() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_isFirstLaunch, false);
+  }
+}
